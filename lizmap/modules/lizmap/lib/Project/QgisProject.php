4e15e1359e17d37516f89f7b2eb73efc831f08a5
rldhont <rldhont@gmail.com>
2023-10-25 10:49:10 +0200

    QgisProjectParser with \XMLReader

--- lizmap/modules/lizmap/lib/Project/QgisProject.php
+++ lizmap/modules/lizmap/lib/Project/QgisProject.php
@@ -1267,6 +1267,9 @@ class QgisProject
         list($this->relations, $this->relationsFields) = $this->readRelations($qgsXml);
     }
 
+    /**
+     * @param \SimpleXMLElement $xml
+     */
     protected function readWMSInformation($qgsLoad)
     {
 
